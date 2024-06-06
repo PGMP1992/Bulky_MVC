0a3fe2a6406d194aab682fc05ef879a16eb1f224
PGMP1992 <pedro@dkltd.net>
2024-06-06 12:11:08 +0200

    Added Home.Details View

--- BulkyWeb/Areas/Customer/Controllers/HomeController.cs
+++ BulkyWeb/Areas/Customer/Controllers/HomeController.cs
@@ -24,9 +24,9 @@ namespace BulkyWeb.Areas.Customer.Controllers
             return View(productList);
         }
 
-        public IActionResult Details(int id)
+        public IActionResult Details(int productId)
         {
-            Product product = _unitOfWork.Product.Get(p=>p.Id==id, includeProperties: "Category");
+            Product product = _unitOfWork.Product.Get(p=>p.Id==productId, includeProperties: "Category");
             return View(product);
         }
 
