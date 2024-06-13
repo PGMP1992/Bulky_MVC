cda2208b11e85404524c695264de74220e9fcc1d
PGMP1992 <pedro@dkltd.net>
2024-06-13 10:56:42 +0200

    Created repository for OrderHeader and OrderDetail

---
+++ Bulky.DataAccess/Repository/OrderDetailRepository.cs
@@ -0,0 +1,20 @@
+﻿using Bulky.DataAccess.Repository.IRepository;
+using Bulky.Models;
+
+namespace Bulky.DataAccess.Repository
+{
+    public class OrderDetailRepository : Repository<OrderDetail>, IOrderDetailRepository
+    {
+        private ApplicationDBContext _db;
+
+        public OrderDetailRepository(ApplicationDBContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(OrderDetail obj)
+        {
+            _db.OrderDetails.Update(obj);
+        }
+    }
+}
---
+++ Bulky.DataAccess/Repository/OrderHeaderRepository.cs
@@ -0,0 +1,20 @@
+﻿using Bulky.DataAccess.Repository.IRepository;
+using Bulky.Models;
+
+namespace Bulky.DataAccess.Repository
+{
+    public class OrderHeaderRepository : Repository<OrderHeader>, IOrderHeaderRepository
+    {
+        private ApplicationDBContext _db;
+
+        public OrderHeaderRepository(ApplicationDBContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(OrderHeader obj)
+        {
+            _db.OrderHeaders.Update(obj);
+        }
+    }
+}
