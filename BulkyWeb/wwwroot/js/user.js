659cb3f60a8ce3da5c10c80244c1e01d12712c81
PGMP1992 <pedro@dkltd.net>
2024-08-01 10:22:56 +0200

    Fixed bug on User Management and register page

--- BulkyWeb/wwwroot/js/user.js
+++ BulkyWeb/wwwroot/js/user.js
@@ -23,10 +23,10 @@ function loadDataTable() {
                         return `
                         <div class="text-center">
                              <a onclick=LockUnlock('${data.id}') class="btn btn-danger text-white" style="cursor:pointer; width:100px;">
-                                    <i class="bi bi-lock-fill"></i>  Lock
+                                    <i class="bi bi-unlock-fill"></i>UnLock
                                 </a> 
                                 <a href="/admin/user/RoleManagement?userId=${data.id}" class="btn btn-danger text-white" style="cursor:pointer; width:150px;">
-                                     <i class="bi bi-pencil-square"></i> Permission
+                                     <i class="bi bi-pencil-square"></i>Permission
                                 </a>
                         </div>
                     `
@@ -35,10 +35,10 @@ function loadDataTable() {
                         return `
                         <div class="text-center">
                               <a onclick=LockUnlock('${data.id}') class="btn btn-success text-white" style="cursor:pointer; width:100px;">
-                                    <i class="bi bi-unlock-fill"></i>  UnLock
+                                    <i class="bi bi-lock-fill"></i>Lock
                                 </a>
                                 <a href="/admin/user/RoleManagement?userId=${data.id}" class="btn btn-danger text-white" style="cursor:pointer; width:150px;">
-                                     <i class="bi bi-pencil-square"></i> Permission
+                                     <i class="bi bi-pencil-square"></i>Permission
                                 </a>
                         </div>
                     `
