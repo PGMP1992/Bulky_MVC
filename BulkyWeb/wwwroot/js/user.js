6d5e1f280fb3376fe373dd3b254d1abe6e2a7373
Pedro Martinez <pedro@dkltd.net>
2025-04-10 10:09:30 +0200

    Many layout changes in Customer area. Changed both Login and register pages

--- BulkyWeb/wwwroot/js/user.js
+++ BulkyWeb/wwwroot/js/user.js
@@ -34,10 +34,10 @@ function loadDataTable() {
                     else {
                         return `
                         <div class="text-center">
-                              <a onclick=LockUnlock('${data.id}') class="btn btn-success text-white" style="cursor:pointer; width:50px;">
+                              <a onclick=LockUnlock('${data.id}') class="btn btn-danger text-white" style="cursor:pointer; width:50px;">
                                     <i class="bi bi-lock-fill"></i>
                                 </a>
-                                <a href="/admin/user/RoleManagement?userId=${data.id}" class="btn btn-danger text-white" style="cursor:pointer; width:50px;">
+                                <a href="/admin/user/RoleManagement?userId=${data.id}" class="btn btn-success text-white" style="cursor:pointer; width:50px;">
                                      <i class="bi bi-pencil-square"></i>
                                 </a>
                         </div>
